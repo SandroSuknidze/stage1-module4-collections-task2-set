438fdf4babe60699a7e6ba68c569de1185a66030
Sandro Ubuntu <sandro.suknidze.1@btu.edu.ge>
2024-11-04 13:35:38 +0400

    #1

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -1,9 +1,25 @@
 package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> result = new LinkedHashSet<>();
+        for (String first : firstSet) {
+            if (secondSet.contains(first) && !thirdSet.contains(first)) {
+                result.add(first);
+            }
+        }
+
+        for (String third : thirdSet) {
+            if (!firstSet.contains(third) && !secondSet.contains(third)) {
+                result.add(third);
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.set;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        Set<Integer> result = new TreeSet<>();
+
+        for (Integer num : sourceList) {
+            if (num * num <= upperBound && num * num >= lowerBound) {
+                result.add(num * num);
+            }
+        }
+
+        return result;
     }
 }
